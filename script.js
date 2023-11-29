65b5f924aab041b731333ea3297269cac35380dc
Omar101-jpg <144621174+Omar101-jpg@users.noreply.github.com>
2023-11-29 14:03:13 -0800

    Finished

--- script.js
+++ script.js
@@ -1,43 +1,36 @@
-/* Assignment 04: Finishing a Todo List App
- *
- * 
- *
- */
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
 
+function addTask(){
+  if(inputBox.value === ''){
+  alert("You must write something"); }
+else{
+let li = document.createElement("li");
+li.innerHTML = inputBox.value;
+listContainer.appendChild(li);
+let span = document.createElement("span");
+span.innerHTML = "\u00d7";
+li.appendChild(span);
+ }
+inputBox.value = "";
+saveData();
+ }
 
-//
-// Variables
-//
-
-// Constants
-const appID = "app";
-const headingText = "To do. To done. ✅";
-
-// DOM Elements
-let appContainer = document.getElementById(appID);
-
-//
-// Functions
-//
-
-// Add a heading to the app container
-function inititialise() {
-  // If anything is wrong with the app container then end
-  if (!appContainer) {
-    console.error("Error: Could not find app contianer");
-    return;
+ listContainer.addEventListener("click", function(e){
+  if(e.target.tagName === "LI"){
+    e.target.classList.toggle("checked");
+    saveData();
   }
+  else if(e.target.tagName === "SPAN"){
+    e.target.parentElement.remove();
+    saveData();
+  }
+ }, false);
 
-  // Create an h1 and add it to our app
-  const h1 = document.createElement("h1");
-  h1.innerText = headingText;
-  appContainer.appendChild(h1);
-
-  // Init complete
-  console.log("App successfully initialised");
-}
-
-//
-// Inits & Event Listeners
-//
-inititialise();
+ function saveData(){
+  localStorage.setItem("data", listContainer.innerHTML);
+ }
+ function showTask(){
+  listContainer.innerHTML = localstorage.getItem("data");
+ }
+ showTask();
